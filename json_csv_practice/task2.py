6255ea1505781d7e7978f91bba416646e203a10d
Vilis322 <viliskrill322@gmail.com>
2025-05-07 22:56:48 +0300

    added task 2

--- json_csv_practice/task2.py
+++ json_csv_practice/task2.py
@@ -1,3 +1,64 @@
+from pathlib import Path
+from csv import DictReader
+from collections import defaultdict
+from logging import getLogger
+from datetime import datetime
+
+
 def task2() -> None:
-    """"""
-    pass
+    """Analyzes sales data from a CSV file and displays statistic."""
+    print("\n=== TASK 2 STARTS ===")
+    logger = getLogger("task2")
+    logger.info("Task 2 started")
+
+    file_path = Path("sales.csv")  # Starts Step 1: reading data from file
+
+    try:
+        if not file_path.exists():
+            raise FileNotFoundError(f"File '{file_path}' not found.")
+
+        with file_path.open(encoding='utf-8') as file:
+            reader = DictReader(file, skipinitialspace=True)
+            sales = [row for row in reader]
+    except FileNotFoundError as e:
+        logger.error(f"Error opening file '{file_path}': {e}")
+        print(f"{e} Check the file path and try again.")
+        return
+    except Exception as e:
+        logger.error(f"Error reading file '{file_path}': {e}")
+        print(f"An error occurred while reading file '{file_path}'. Check the file and try again.")
+        return
+
+    if not sales:
+        logger.warning("Sales file loaded but contains no data")
+        logger.info("Task 2 stopped")
+        print(f"File '{file_path}' is empty. Check the file and try again.")
+        return
+
+    # Ends Step 1, starts Step 2, 3, 4: counting amount of sales during the whole period,
+    # defining top-selling item, dividing sales by months and printing it
+    total_sales = 0
+    total_sale_per_item = defaultdict(int)
+    monthly_total_sales = defaultdict(int)
+
+    for i, row in enumerate(valid_sales):
+        try:
+            item = row["Item"]
+            amount = int(row["Sum"])
+            date = row["Date"]
+            date_obj = datetime.strptime(date, "%Y-%m-%d")
+            monthly_key = date_obj.strftime("%Y-%m")
+
+            total_sales += amount
+            total_sale_per_item[item] += amount
+            monthly_total_sales[monthly_key] += amount
+        except (KeyError, ValueError) as e:
+            logger.warning(f"Skipping invalid row #{i}: {e}. Row: {row}")
+            continue
+
+    top_item = max(total_sale_per_item.items(), key=lambda x: x[1])
+    print(f"\nTotal sales sum: {total_sales}¥")
+    print(f"\nTop-selling item: {top_item[0]}. Total sales: {top_item[1]}¥")
+    print(f"\nMonthly sales totals:")
+    for month, amount in sorted(monthly_total_sales.items()):
+        print(f"{month}: {amount}¥")
